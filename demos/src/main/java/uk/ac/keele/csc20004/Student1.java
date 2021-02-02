c0bddc7531aabda8174ce18bfc64ba7ca85511de
marcoortolani <marco.ortolani@gmail.com>
2021-02-02 16:48:43 +0000

    File renaming, and added javadoc

--- demos/src/main/java/uk/ac/keele/csc20004/Student1.java
+++ demos/src/main/java/uk/ac/keele/csc20004/Student1.java
@@ -5,14 +5,14 @@
 package uk.ac.keele.csc20004;
 
 /**
- * A class for defining a student as a pair of their ID and name.
+ * A class  defining a student as a pair of their ID and name.
  * Student objects are immutable (the fields are final).
  *
  * In this implementation, equals() and hashcode() are *not* overridden, so
- * those objects will make use of the default Ob
+ * those objects will make use of the default equals() method as provided by Object.
  *
  * @see java.lang.Object#equals(Object)
- * @see HashcodeEquals
+ * @see HashcodeDemo
  *
  * @author Marco Ortolani
  */
@@ -21,7 +21,8 @@ public class Student1 {
     private final String name;
 
     /**
-     *
+     * Constructor for a Student object, with obvious meaning.
+     * 
      * @param id an integer representing the student's ID
      * @param name a String representing the student's name
      */
--- demos/src/main/java/uk/ac/keele/csc20004/TreeSetDemo.java
+++ demos/src/main/java/uk/ac/keele/csc20004/TreeSetDemo.java
@@ -7,6 +7,14 @@ package uk.ac.keele.csc20004;
 import java.util.Comparator;
 import java.util.TreeSet;
 
+/**
+ * An implementation of Comparator that relies on the 
+ * original comparison of Strings (lexicographic order, i.e. like 
+ * the dictionary)
+ * 
+ * @see java.util.Comparator
+ * @see java.lang.String#compareTo(String)
+ */
 class MyComparator1 implements Comparator<String> { 
     public int compare(String str1, String str2) { 
     String first_Str = str1 ; 
@@ -16,6 +24,12 @@ class MyComparator1 implements Comparator<String> {
     } 
 }
 
+/**
+ * An implementation of Comparator that compares Strings only
+ * wrt to their relative lenghts (so Strings with the same lenght are "equal")
+ * 
+ * @see java.util.Comparator
+ */
 class MyComparator2 implements Comparator<String> { 
     public int compare(String str1, String str2) { 
         if (str1.length() < str2.length())
@@ -27,9 +41,19 @@ class MyComparator2 implements Comparator<String> {
     } 
 }
 
+/**
+ * An example of use of the TreeSet class, with Comparator.
+ * 
+ * @see java.util.TreeSet
+ * @see java.util.Comparator
+ * 
+ * @author Marco Ortolani
+ */
 public class TreeSetDemo { 
     public static void main(String[] args) 
     { 
+        // Declare a set, as a TreeSet, providing the object implementing
+        // the comparator ()
         TreeSet<String> tree_set = new TreeSet<String>(new MyComparator1()); 
    
         tree_set.add("hello"); 
